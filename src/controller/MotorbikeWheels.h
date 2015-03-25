721fe7876303716afe39924b8fa4473f1a78e8a8
Max <maxim.morozov.a@gmail.com>
2015-03-25 23:36:51 +0300

    Segway model has been recalculated with new parameters

--- src/controller/MotorbikeWheels.h
+++ src/controller/MotorbikeWheels.h
@@ -14,12 +14,12 @@ namespace ev3way {
 
 class MotorbikeWheels {
 protected:
-	static constexpr float K_F1 = -0.824844186088244F * Constants::DEG2RAD;
-	static constexpr float K_F2 = -41.4074441149571F * Constants::DEG2RAD;
-	static constexpr float K_F3 = -1.32276811679559F * Constants::DEG2RAD;
-	static constexpr float K_F4 = -4.40904857993352F * Constants::DEG2RAD;
+	static constexpr float K_F1 = -0.825481360095411F * Constants::DEG2RAD;
+	static constexpr float K_F2 = -40.3738849807044F * Constants::DEG2RAD;
+	static constexpr float K_F3 = -1.32450621391743F * Constants::DEG2RAD;
+	static constexpr float K_F4 = -4.22675237197456F * Constants::DEG2RAD;
 
-	static constexpr float K_I = -0.417915074452005F * Constants::DEG2RAD; // servo control integral gain
+	static constexpr float K_I = -0.417669314488814F * Constants::DEG2RAD; // servo control integral gain
 
 	static constexpr float K_THETADOT = 6.31578947368421F / Constants::DEG2RAD;   // forward target speed gain 0.2 m/s
 };
